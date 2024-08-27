aab29b73e33405ab7f1bc54e1bb961e6ce5beeb2
Jeevesh27 <123328044+Jeevesh27@users.noreply.github.com>
2024-08-27 08:15:05 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
-  const videoID = "v9yg37oV69I";
+  const videoID = "zo7JFCT0MVs";
   const arr = Array(50).fill(`https://www.youtube.com/embed/${videoID}?autoplay=1&mute=1&loop=1&playlist=${videoID}&vq=small`);
 
   return (
